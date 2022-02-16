94725b236c30338213971bbf62a6fb7c6ca8e866
bitoy88 <rheyson88@gmail.com>
2022-02-16 10:26:32 +0800

    Mini_CRM

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::truncate();
+        User::create([
+            
+            'name' => 'Reyson',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('password'),
+            'remember_token' => Str::random(60)
+        ]);
+    }
+}
